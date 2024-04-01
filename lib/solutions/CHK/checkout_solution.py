80368fe8b10ee64af2bf145e3299d2ebff546494
sbx_user1051 <sbx_user1051@169.254.55.5>
2024-04-01 13:04:46 +0000

    Mon Apr 01 13:04:46 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -7,18 +7,37 @@ def checkout(skus):
         'A': 0,
         'B': 0,
         'C': 0,
-        'D': 0
+        'D': 0,
+        'E': 0,
     }
     for char in skus:
         if not char in counts.keys():
             return -1
         counts[char] += 1
 
+    free_items = {
+        'A': 0,
+        'B': 0,
+        'C': 0,
+        'D': 0,
+        'E': 0,
+    }
+
+    # For now, will always be cheaper to deduct the free item
+    counts['B'] -= max(counts['E'] // 2, 0)
+
     sum = 0
-    sum += (counts['A'] // 3) * 130 + (counts['A'] % 3) * 50
+
+    sum += (counts['A'] // 5) * 500
+    counts['A'] -= counts['A'] // 5
+    sum += (counts['A'] // 3) * 130
+    counts['A'] -= counts['A'] // 3
+    sum += counts['A'] * 50
+
     sum += (counts['B'] // 2) * 45 + (counts['B'] % 2) * 30
     sum += counts['C'] * 20
     sum += counts['D'] * 15
 
     return sum 
+
 
--- test/solution_tests/CHK/test_chk1.py
+++ test/solution_tests/CHK/test_chk1.py
@@ -7,3 +7,6 @@ class TestChk():
         assert checkout_solution.checkout("") == 0
         assert checkout_solution.checkout('aa') == -1
         assert checkout_solution.checkout("-") == -1
+
+        assert checkout_solution.checkout("AAAAAAAAABEE") == 460 # 9A, 2E (B is free)=> 200+130+50+80
+
