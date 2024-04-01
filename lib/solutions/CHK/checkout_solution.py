4405fd2722ac4ff192f48e8492490a01792254fd
sbx_user1051 <sbx_user1051@169.254.55.5>
2024-04-01 13:36:52 +0000

    Mon Apr 01 13:36:52 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,14 +3,8 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    counts = {
-        'A': 0,
-        'B': 0,
-        'C': 0,
-        'D': 0,
-        'E': 0,
-        'F': 0
-    }
+    counts = {k: 0 for k in "ABCDEFGHIJKLMNOPQRSTUVWXYZ"}
+
     for char in skus:
         if not char in counts.keys():
             return -1
@@ -21,20 +15,64 @@ def checkout(skus):
 
     counts['F'] = max(counts['F'] - counts['F'] // 3, 0)
 
+    counts['M'] = max(counts['M'] - counts['N'] // 3, 0)
+
+    counts['Q'] = max(counts['Q'] - counts['R'] // 3, 0)
+
+    counts['U'] = max(counts['U'] - counts['U'] // 4, 0)
+
     sum = 0
 
-    sum += (counts['A'] // 5) * 200
-    counts['A'] = counts['A'] % 5
-    sum += (counts['A'] // 3) * 130
-    counts['A'] = counts['A'] % 3
-    sum += counts['A'] * 50
+    sum += triple_price(counts['A'], 50, 3, 130, 5, 200)
 
-    sum += (counts['B'] // 2) * 45 + (counts['B'] % 2) * 30
+    sum += double_price(counts['B'], 30, 2, 45)
+
     sum += counts['C'] * 20
     sum += counts['D'] * 15
     sum += counts['E'] * 40
     sum += counts['F'] * 10
+    sum += counts['G'] * 20
+
+    sum += triple_price(counts['H'], 10, 5, 45, 10, 80)
+
+    sum += counts['I'] * 35
+    sum += counts['J'] * 60
+    
+    sum += double_price(counts['K'], 80, 2, 150)
+
+    sum += counts['L'] * 90
+
+    sum += counts['M'] * 15
+    sum += counts['N'] * 40 
+    sum += counts['O'] * 10
+    
+    sum += double_price(counts['P'], 50, 5, 200)
+    
+    sum += double_price(counts['Q'], 30, 3, 80)
+
+    sum += counts['R'] * 50
+    sum += counts['S'] * 30
+    sum += counts['T'] * 20
+    sum += counts['U'] * 40
+    
+    sum += triple_price(counts['V'], 50, 2, 90, 3, 130)
+
+    sum += counts['W'] * 20
+    sum += counts['X'] * 90
+    sum += counts['Y'] * 10
+    sum += counts['Z'] * 50
 
     return sum 
 
-checkout("EE")
+
+def double_price(count, base_price, volume1, price1):
+    return (count // volume1) * price1 + (count % volume1) * base_price
+
+def triple_price(count, base_price, volume1, price1, volume2, price2):
+    sum = 0
+    sum += (count // volume2) * price2
+    count = count % volume2
+    sum += (count // volume1) * price1
+    count = count % volume1
+    sum += count * base_price
+    return sum
